611b9862dacb5492a9e10fe898a30c463cd6dd6b
Neftalí Yagua <despacho@neftaliyagua.com>
2020-11-02 13:24:16 -0500

    Oazuth twitter

--- assets/firebase/init-firebase.js
+++ assets/firebase/init-firebase.js
@@ -18,7 +18,7 @@ var uiConfig = {
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       'microsoft.com',
-      //      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
 //      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
