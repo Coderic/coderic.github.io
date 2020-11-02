d2156a8316480eb08dad550eeae727ebb4272d7a
Neftalí Yagua <despacho@neftaliyagua.com>
2020-11-02 01:10:26 -0500

    Agregar configuracion firebase

--- assets/firebase/init-firebase.js
+++ assets/firebase/init-firebase.js
@@ -19,7 +19,7 @@ var uiConfig = {
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
 //      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
 //      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
 //      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
