4e1d2e41a8599be4c3d4f61a4fbb895afeeebb45
Neftalí Yagua <despacho@neftaliyagua.com>
2020-11-02 00:46:41 -0500

    Agregar configuracion firebase

--- assets/firebase/init-firebase.js
+++ assets/firebase/init-firebase.js
@@ -8,5 +8,32 @@ var firebaseConfig = {
     appId: "1:204370804567:web:46b70244eb62efe59a0812"
 };
 var defaultProject = firebase.initializeApp(firebaseConfig);
-
 console.log(defaultProject.name);
+var defaultStorage = firebase.storage();
+var defaultFirestore = firebase.firestore();
+var uiConfig = {
+    signInSuccessUrl: 'https://coderic.net/',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+//      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    tosUrl: 'https://coderic.net/terminos',
+    // Privacy policy url/callback.
+    privacyPolicyUrl: function() {
+      window.location.assign('https://coderic.net/privacidad');
+    }
+  };
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
